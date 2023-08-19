8cd93b41a13abfb4ffe3483666781b98b531b563
Satyam <satyam@satyam.com.ar>
2023-08-19 18:04:39 +0200

    Added some cleanup of unwanted styling in `toHexo.mjs`. Removes empty spans often produced by removing such styling

--- src/toHexo.mjs
+++ src/toHexo.mjs
@@ -7,18 +7,105 @@ import { shouldUpdate } from './utils.mjs';
 
 import { __dirname, SRC_DIRS, DEST_DIRS } from './constants.mjs';
 import { emptyDir } from 'fs-extra';
-
+import { parse } from 'node-html-parser';
 const dataFiles = await globby(join(SRC_DIRS.JSON_FILES, '**/*.json'));
 
 const HEXO = join(__dirname, '../hexo');
 
+const specs = {};
+
+const invalidStyles = [
+  'margin-left',
+  'text-shadow',
+  'font-family',
+  'color',
+  'letter-spacing',
+  'line-height',
+  'background-color',
+];
+function clean(content, name) {
+  const data = parse(
+    content
+      .replaceAll('<span></span>', '')
+      .replaceAll('<span></span>', '')
+      .replaceAll('<p></p>', '')
+  );
+
+  for (const el of data.querySelectorAll('[style]')) {
+    const style = el.getAttribute('style').trim();
+    const newStyle = [];
+    style.split(';').forEach((spec) => {
+      if (spec.trim() === '') return;
+      const [prop, value] = spec
+        .replaceAll('\\n', ' ')
+        .replaceAll(/\s+/g, ' ')
+        .split(':')
+        .map((p) => p.trim());
+      if (!invalidStyles.includes(prop)) {
+        newStyle.push(`${prop}:${value}`);
+      }
+    });
+    if (newStyle.length) {
+      el.setAttribute('style', newStyle.join(';'));
+    } else {
+      el.removeAttribute('style');
+    }
+  }
+  for (const el of data.querySelectorAll('span:empty')) {
+    el.remove();
+  }
+
+  for (let changed = true; changed; ) {
+    changed = false;
+    for (const el of data.querySelectorAll('span')) {
+      if (Object.keys(el.attributes).length === 0) {
+        if (
+          typeof el.firstChild.tagName === 'undefined' &&
+          typeof el.firstChild.nextSibling === 'undefined'
+        ) {
+          el.replaceWith(el.innerText);
+          changed = true;
+        }
+        if (
+          el.firstChild.tagName === 'SPAN' &&
+          el.firstChild.nextSibling === null
+        ) {
+          el.replaceWith(el.firstChild.outerHTML);
+          changed = true;
+        }
+      }
+    }
+  }
+  for (const el of data.querySelectorAll('[style]')) {
+    const style = el.getAttribute('style').trim();
+    style.split(';').forEach((spec) => {
+      const [prop, value] = spec
+        .replaceAll('\\n', ' ')
+        .replaceAll(/\s+/g, ' ')
+        .split(':')
+        .map((p) => p.trim());
+      if (!(prop in specs)) {
+        specs[prop] = {};
+      }
+      if (!(value in specs[prop])) {
+        specs[prop][value] = 1;
+      } else {
+        specs[prop][value] += 1;
+      }
+    });
+  }
+  return data.innerHTML;
+}
 await emptyDir(join(HEXO, 'source'));
 await ensureDir(join(HEXO, 'source', '_posts'));
 for (const dataFile of dataFiles) {
   const data = await readJson(dataFile);
 
-  const { name } = data;
+  const { name, content } = data;
   if (!name || name.startsWith('/blog')) continue;
+
+  const cleanedContent = clean(content, name);
+
   console.log(name);
 
   if (name.startsWith('/single-post')) {
@@ -45,7 +132,7 @@ ${stringify({
   excerpt: data.description?.replaceAll('\n', ' '),
 })}
 ---
-${data.content}`
+${cleanedContent}`
     );
 
     continue;
@@ -61,10 +148,10 @@ ${stringify({
   updated: data.modified_time,
 })}
 ---
-${data.content}
+${cleanedContent}
    `
   );
-  console.log(name, destFile);
+  // console.log(name, destFile);
 }
 
 // Copy images
